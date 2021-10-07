2be918a375edddacd9a8700404033b38f9253913
adarsh <adarshkushwaha296@gmail.com>
2021-10-07 08:33:50 +0530

    deploying

--- src/Component/Form/Form.jsx
+++ src/Component/Form/Form.jsx
@@ -1,26 +1,26 @@
-// import React from 'react'
-// import classes from "./Form.module.css"
+import React from 'react'
+import classes from "./Form.module.css"
 
-// const Form = () => {
-//     return (
-//         <form>
-//             <div className={classes.controlGroup}>
-//                 <div className={classes.name}>
-//                     <div>
-//                         <label htmlFor="fname">First Name</label>
-//                         <input type="text" id="fname" />
-//                     </div>
-//                     <div>
-//                         <label htmlFor="lname">Last Name</label>
-//                         <input type="text" id="lname" />
-//                     </div>
-//                 </div>
+const Form = () => {
+    return (
+        <form>
+            <div className={classes.controlGroup}>
+                <div className={classes.name}>
+                    <div>
+                        <label htmlFor="fname">First Name</label>
+                        <input type="text" id="fname" />
+                    </div>
+                    <div>
+                        <label htmlFor="lname">Last Name</label>
+                        <input type="text" id="lname" />
+                    </div>
+                </div>
 
-//             </div>
+            </div>
 
-//         </form>
+        </form>
 
-//     )
-// }
+    )
+}
 
-// export default Form;
+export default Form;
